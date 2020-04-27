96f04531aca14ea345bbb92e8f67e34b9d5c13a1
“ <904426354@qq.com>
2020-04-27 17:34:43 +0800

    managesys项目---》提交分支恢复

--- src/main/java/com/renting/common/Constants.java
+++ src/main/java/com/renting/common/Constants.java
@@ -2,9 +2,7 @@ package com.renting.common;
 
 import org.springframework.stereotype.Component;
 
-/**
- * 测试分支
- */
+
 @Component
 public class Constants {
     public final static String ADMIN_SESSIONID="87957695779572";

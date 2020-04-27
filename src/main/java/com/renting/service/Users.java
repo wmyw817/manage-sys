82eef916808d0838c983aa6344e8af9ee262735b
“ <904426354@qq.com>
2020-04-27 16:35:00 +0800

    managesys项目---》恢复原样

--- src/main/java/com/renting/service/Users.java
+++ src/main/java/com/renting/service/Users.java
@@ -3,10 +3,7 @@ package com.renting.service;
 import com.renting.entity.User;
 
 public interface Users {
-    /**
-     * 测试
-     * @return
-     */
+
     public User selectAllInfo();
     public User UserInfo(String uname);
 }

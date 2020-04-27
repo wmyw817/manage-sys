a8c3e459fe02bc8cb6b6d3a20b8923fe6b81d9a8
“ <904426354@qq.com>
2020-04-27 16:48:59 +0800

    managesys项目---》提交测试一

--- src/main/java/com/renting/service/Users.java
+++ src/main/java/com/renting/service/Users.java
@@ -3,7 +3,10 @@ package com.renting.service;
 import com.renting.entity.User;
 
 public interface Users {
-
+    /**
+     * 修改一
+     * @return
+     */
     public User selectAllInfo();
     public User UserInfo(String uname);
 }
